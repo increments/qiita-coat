0f40e6d8136e876ee1280993df0ceb0b01e546bb
Ryo Nakamura <r7kamura@gmail.com>
2015-08-18 02:29:28 +0900

    Add markdown docs at GET /markdown

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source "https://rubygems.org"
 
 gem "scss_lint"
 gem "sinatra"
+gem "sinatra-contrib"
 gem "sitespec"
--- lib/qiita/coat/application.rb
+++ lib/qiita/coat/application.rb
@@ -1,12 +1,21 @@
 require "sinatra/base"
+require "sinatra/reloader"
 
 module Qiita
   module Coat
     class Application < Sinatra::Base
+      configure :development do
+        register Sinatra::Reloader
+      end
+
       set :root, File.expand_path("../../../..", __FILE__)
 
       get "/" do
         slim :"roots/show"
+      end
+
+      get "/markdown" do
+        slim :"docs/markdown"
       end
     end
   end
