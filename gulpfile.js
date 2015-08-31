957fb6f9db2f7c7fd1c9966ce50635926396f02c
r7kamura <r7kamura@gmail.com>
2015-08-31 11:24:14 +0900

    Remove unused javascript task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 var ghPages = require('gulp-gh-pages');
 var gulp = require('gulp');
-var path = require('path');
 var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
 var watch = require('gulp-watch');
 var webpack = require('webpack-stream');
 
@@ -14,27 +12,6 @@ gulp.task('deploy-pages', function() {
   return gulp.src('./build/**/*')
     .pipe(ghPages(options));
 });
-
-gulp.task(
-  'minify',
-  function () {
-    gulp.src('assets/javascripts/application.js')
-      .pipe(
-        webpack({
-          output: {
-            filename: 'application.js'
-          },
-          resolve: {
-            root: [
-              path.join(__dirname, "bower_components")
-            ]
-          }
-        })
-      )
-      .pipe(uglify())
-      .pipe(gulp.dest('public/javascripts'));
-  }
-);
 
 gulp.task(
   'sass',
@@ -52,4 +29,4 @@ gulp.task(
   }
 );
 
-gulp.task('default', ['minify', 'sass']);
+gulp.task('default', ['sass']);
